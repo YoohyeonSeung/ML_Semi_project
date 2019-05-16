5c26659ce795bd3a9402637cd9b20b00affe1e52
Hyeon-seung <ykh4016@gmail.com>
2019-05-17 01:58:56 +0900

    Modified code has error that exceeds 10% of system memory

--- HS_skin_cancer/Skin_cancer_CNN_Under_construction.py
+++ HS_skin_cancer/Skin_cancer_CNN_Under_construction.py
@@ -4,10 +4,9 @@ import os
 import shutil
 import keras.optimizers as op
 import keras.initializers as kini
+import keras.backend as K
 
 
-
-from PIL import Image
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.layers import Flatten
@@ -45,49 +44,78 @@ from keras import losses
 #         to_dir = "C:/workspaces/ML_Semi_project/HS_skin_cancer/skin_cancer_data/moved_test_data/" + revised_dx[i] + "/"
 #         shutil.move(src + filename, to_dir + filename)
 
+K.clear_session() # 새로운 세션으로 시작
 
 train_datagen = ImageDataGenerator(rescale=1./255)
 
+train_dir = 'C:/workspaces/ML_Semi_project/HS_skin_cancer/skin_cancer_data/moved_train_data'
+test_dir =  'C:/workspaces/ML_Semi_project/HS_skin_cancer/skin_cancer_data/moved_test_data'
+
+
 train_generator = train_datagen.flow_from_directory(    # train
-    'C:/workspaces/ML_Semi_project/HS_skin_cancer/skin_cancer_data/moved_train_data',
+    train_dir ,
     target_size=(600, 450),
     batch_size = 10,
-    class_mode = 'categorical'
+    class_mode = 'categorical',
+    shuffle = True
 )
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 test_generator = test_datagen.flow_from_directory(     # test
-        'C:/workspaces/ML_Semi_project/HS_skin_cancer/skin_cancer_data/moved_test_data',
+        test_dir,
         target_size=(600, 450),
         batch_size=10,
-        class_mode='categorical')
+        class_mode='categorical',
+        shuffle = False
+)
 
 model = Sequential()
-model.add(Conv2D(4, kernel_size=(5, 3),
+model.add(Conv2D(4, kernel_size=(9, 6),
                  strides=1,
                  kernel_initializer=kini.he_normal(seed=None),
                  activation="relu",
-                 input_shape=(600, 450, 3)))
+                 input_shape=(600, 450, 3))
+          )
+model.add(Conv2D(4, kernel_size=(9, 6),
+                 strides=1,
+                 kernel_initializer=kini.he_normal(seed=None),
+                 activation="relu",
+                 input_shape=(600, 450, 3))
+          )
 model.add(MaxPooling2D(pool_size=(2, 2)))
+# 1st conv
+
+model.add(Conv2D(6, (7, 3),
+                 strides=1,
+                 kernel_initializer=kini.he_normal(seed=None),
+                 activation="relu"))
+model.add(Conv2D(6, (7, 3),
+                 strides=1,
+                 kernel_initializer=kini.he_normal(seed=None),
+                 activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+# 2nd conv
 
 model.add(Conv2D(8, (5, 3),
                  strides=1,
                  kernel_initializer=kini.he_normal(seed=None),
                  activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(16, (5, 3),
-                 strides=2,
-                 kernel_initializer=kini.he_normal(seed=None),
-                 activation="relu"))
-
-model.add(Conv2D(32, (3, 2),
-                 strides= 2,
+model.add(Conv2D(8, (5, 3),
+                 strides= 1,
                  kernel_initializer=kini.he_normal(seed=None),
                  activation="relu"),
                  )
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
 model.add(Flatten())
-# model.add(Dropout(0.25))
+model.add(Dropout(0.7))
+model.add(Dense(4096,
+                 kernel_initializer=kini.he_normal(seed=None),
+                 activation="relu"))
+model.add(Dense(2048,
+                 kernel_initializer=kini.he_normal(seed=None),
+                 activation="relu"))
 model.add(Dense(1024,
                  kernel_initializer=kini.he_normal(seed=None),
                  activation="relu"))
@@ -108,7 +136,7 @@ model.compile(loss=losses.categorical_crossentropy, optimizer=op.Adam(lr=1e-4), 
 history = model.fit_generator(
         train_generator,
         steps_per_epoch=10,
-        epochs=5)
+        epochs=40)
 
 
 print("-- Evaluate --")
@@ -139,6 +167,7 @@ plt.legend(['train'], loc='upper left')
 plt.show()
 
 
+# print(model.summary())
 
 
 
--- Test/test.py
+++ Test/test.py
@@ -1,1 +1,39 @@
-print("Hello, World")
+def conv(h, fh, w, fw, s):
+    return ((h-fh)/s)+1, ((w-fw)/s)+1
+
+def pooling(h, w, sh, sw):
+    return h/sh, w/sw
+
+
+h, w = conv(600, 9, 450, 6, 1)
+print(h, w)     # 592,  445
+
+
+h, w = conv(h, 9, w, 6, 1)
+print(h, w)   # 584, 440
+
+h, w = pooling(h, w, 2, 2)
+print(h, w)     #292, 220
+print('1st conv layer')
+
+h, w = conv(h, 7, w, 3, 1)
+print(h, w)    # 286, 218
+
+h, w = conv(h, 7, w, 3, 1)
+print(h, w)    # 280, 216
+
+h, w = pooling(h, w, 2, 2)
+print(h, w)  # 140, 108
+print(" 2nd conv layer " )
+
+h, w = conv(h, 5, w, 3, 1)
+print(h, w)
+h, w = conv(h, 5, w, 3, 1)
+print(h, w)
+h, w = pooling(h, w, 2, 2)
+print(h, w)
+print(" 3rd conv layer " )
+
+
+
+
